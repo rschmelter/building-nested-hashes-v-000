d0b9da04b1a31c7d500f11a9cb9511a379b8fed8
Ryan Schmelter <rschmelter33@gmail.com>
2017-10-28 17:15:27 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -13,7 +13,7 @@ def third_challenge
         #your key/value pairs here
         },
       :hero_friends => []
-   }, 
+   },
    :capulet => {
       :patriarch => {
         name: "Lord Capulet", age: "50"
@@ -31,5 +31,5 @@ def third_challenge
    }
   }
 
-  
+
 end
